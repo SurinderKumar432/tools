0486d9fe8460db444e2adc4232346145fc36342d
LordOfRain <surinder.kumar.432@gmail.com>
2016-09-15 12:18:21 +0530

    Create vmlayout_global.h

---
+++ memory/uvmlayout/include/vmlayout_global.h
@@ -0,0 +1,8 @@
+#ifndef VMLAYOUT_GLOBAL_H__
+#define VMLAYOUT_GLOBAL_H__
+
+#include <vmlayout_support.h>
+#include <vmlayout_elfconst.h>
+#include <vmlayout_list.h>
+
+#endif /* ! VMLAYOUT_GLOBAL_H__ */
